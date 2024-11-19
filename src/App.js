71d882a636f22943c5beb79a49b7e2f3c11b52fe
Pedro Mendonca <104030609+Pedro-Jose-da-Rocha-Mendonca@users.noreply.github.com>
2024-11-19 14:06:54 -0500

    Draft 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Adopt from './components/Adopt';
-import Learn from './components/Learn';
-import Help from './components/Help';
-import PetDetails from './components/PetDetails';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Adopt from './components/Adopt';
+import Help from './components/Help';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/adopt" exact component={Adopt} />
-      <Route path="/learn" component={Learn} />
-      <Route path="/help" component={Help} />
-      <Route path="/pet/:id" component={PetDetails} />
-    </Switch>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/adopt" element={<Adopt />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
