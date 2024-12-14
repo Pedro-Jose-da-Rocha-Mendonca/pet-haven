d76cf1c5211200c61319001e1a3ff2004ee4243e
Pedro Mendonca <pedromendonca2002@gmail.com>
2024-12-14 17:05:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './services/AuthContext';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './styles.css';
+import './assets/styles/styles.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
